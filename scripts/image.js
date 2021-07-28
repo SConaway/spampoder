de8842034c919cb6094abf2e73668997e101fabc
Steven Conaway <steven.conaway@icloud.com>
2021-07-27 21:56:22 -0700

    Add some og: tags

--- scripts/image.js
+++ scripts/image.js
@@ -3,6 +3,7 @@ import sharp from 'sharp'
 const main = async () => {
 
     await sharp('./sammmm.png').toFile('./public/image/image.dzi')
+    await sharp('./sammmm.png').resize({height: 630}).toFile('./public/preview.png')
 }
 
 main()
